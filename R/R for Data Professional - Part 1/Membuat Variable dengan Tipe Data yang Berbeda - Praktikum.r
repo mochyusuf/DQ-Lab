f4baf3632aac58abd80dff70dc8e8f55def5e1b4
Mochamad Yusuf <mochyusuf120@gmail.com>
2025-05-29 06:12:05 +0700

    Membuat Variable dengan Tipe Data yang Berbeda - Praktikum

---
+++ R/R for Data Professional - Part 1/Membuat Variable dengan Tipe Data yang Berbeda - Praktikum.r
@@ -0,0 +1,24 @@
+# Setelah mempelajari dan mencoba soal-soal dari beberapa tipe data yang diberikan oleh Andra, aku berinisiatif untuk praktik langsung mengerjakan case.  
+
+# Tanpa berpikir panjang lagi, aku langsung membuat beberapa variabel dengan tipe data yang berbeda-beda supaya aku bisa lebih memahami konsep dan tipe data yang lebih bervariasi pada bahasa pemrograman R.
+
+# Aku juga membuat catatan terhadap langkah-langkah yang kulakukan dalam bentuk poin-poin agar lebih rapi dan sistematis, seperti di bawah ini :
+
+# Aku membuat variabel bernama data_numerik_1; dan mengisinya dengan angka 7
+# Setelah itu, aku membuat variabel data_numerik_2 dan karena aku sangat menyukai angka 7, jadi aku memberikan nilai 77.7 kepada variabel tersebut.
+# Untuk menyimpan data character, aku membuat variabel bernama data_character dan memberikan nilai "Belajar Data Science Sangat Menyenangkan!"
+# Yang terakhir, untuk data logical, aku memberikan nilai TRUE ke dalam variabel yang bernama data_logical
+
+
+
+#buat variable data_numerik_1 yang berisi 7
+data_numerik_1 <- 7
+
+#buat variable data_numerik_2 yang berisi 77.7
+data_numerik_2 <- 77.7
+
+#buat variable data_character yang berisi "Belajar Data Science Sangat Menyenangkan!"
+data_character <- "Belajar Data Science Sangat Menyenangkan!"
+
+#buat variable data_logical yang berisi TRUE
+data_logical <- "TRUE"
