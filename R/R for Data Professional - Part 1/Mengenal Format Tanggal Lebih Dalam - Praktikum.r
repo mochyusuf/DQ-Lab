7f7605b4c5b2650be98b00ce76333b2d2af2888b
Mochamad Yusuf <mochyusuf120@gmail.com>
2025-05-29 06:17:39 +0700

    Mengenal Format Tanggal Lebih Dalam - Praktikum

---
+++ R/R for Data Professional - Part 1/Mengenal Format Tanggal Lebih Dalam - Praktikum.r
@@ -0,0 +1,24 @@
+# Aku mengernyitkan dahi  sambil mencerna informasi yang baru saja aku dapatkan dari Andra mengenai tipe data tanggal. Masih banyak pertanyaan dalam benakku, tapi aku urung menyampaikannya karena Pak Kroma sudah berteriak dari ruang kerjanya untuk memanggil Andra.
+
+ 
+
+# “Kamu bisa sendiri dulu? Aku ke ruangan Kroma bentar ya, soalnya ada yang urgent,” pamit Andra. 
+
+ 
+
+# “Beres,” kataku menenangkannya. Karena aku masih penasaran dengan dengan tipe data tanggal, aku pun menggerakkan kursor untuk  mencoba mempraktekannya dan mencari tahu sendiri. Aku mulai dengan mengerjakan soal dan perintah yang tersedia berikut ini.
+
+ 
+
+# Isilah format yang tepat untuk tanggal-tanggal yang diberikan melalui Code Editor.
+
+
+as.Date("2021-12-31", "%Y-%m-%d")
+
+as.Date("7 July 2007", "%d %B %Y")
+
+as.Date("1 Jan 70", "%d %b %y")
+
+#Cek tipe data dari my_date
+my_date <- as.Date("Dec 25, 2021", "%b %d, %Y")
+class(my_date)
