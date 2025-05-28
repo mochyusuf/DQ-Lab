fbc4049b7c153612c371e5602e847d022030c90b
Mochamad Yusuf <mochyusuf120@gmail.com>
2025-05-29 06:12:11 +0700

    Apa ya Tipe Data dari Variabel ini - Praktikum

---
+++ R/R for Data Professional - Part 1/Apa ya Tipe Data dari Variabel ini - Praktikum.r
@@ -0,0 +1,26 @@
+# Setelah aku mencoba berbagai macam variabel yang berisi tipe data, aku kembali membuka email yang dikirim oleh Andra, ia mengatakan bahwa pengetahuanku semakin bertambah dalam bahasa pemrograman R.
+
+# Wah, aku senang sekali! Namun, tak bisa kupungkiri masih ada kekeliruan yang kulakukan. Melalui email itu,  Andra kembali mengingatkanku pada kesalahan yang terjadi ketika berusaha menjumlahkan 5 dengan 'tujuh', hasilnya bisa saja keliru karena aku melakukan operasi matematika pada tipe data yang berbeda. 
+
+# Aku mengingatnya agar tidak melakukan kesalahan sama.
+
+# Andra pun memberiku solusi agar aku menggunakan function(x) untuk mengecek tipe data variabel yang sudah aku miliki. Namun karena Andra masih memiliki pekerjaan yang diberikan oleh Pak Kroma ia akan kembali dan membantuku untuk menjelaskan mengenai fungsi class(x).
+
+# Namun, dibanding menunggu Andra aku memilih untuk segera mencoba fungsi class(x) dan mengecek tipe data dari variabel - variabel yang sudah dibuat sebelumnya.
+
+
+#buat variable data_numerik_1 yang berisi 7
+data_numerik_1 <- 7
+class(data_numerik_1)
+
+#buat variable data_numerik_2 yang berisi 77.7
+data_numerik_2 <- 77.7
+class(data_numerik_2)
+
+#buat variable data_character yang berisi "Belajar Data Science Sangat Menyenangkan!"
+data_character <- "Belajar Data Science Sangat Menyenangkan!"
+class(data_character)
+
+#buat variable data_logical yang berisi TRUE
+data_logical <- TRUE
+class(data_logical)
