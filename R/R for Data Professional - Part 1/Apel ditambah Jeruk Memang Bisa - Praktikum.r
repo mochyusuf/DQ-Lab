1723adadc60fac5a7f20d120986a1e281fe76d15
Mochamad Yusuf <mochyusuf120@gmail.com>
2025-05-29 06:11:10 +0700

    Apel ditambah Jeruk Memang Bisa - Praktikum

---
+++ R/R for Data Professional - Part 1/Apel ditambah Jeruk Memang Bisa - Praktikum.r
@@ -0,0 +1,25 @@
+# Halo Sunyi!
+
+# Bagaimana harimu sejauh ini? Apakah kamu sudah merasa jauh lebih familiar dengan bahasa pemrograman R sekarang setelah belajar bersama selama setengah hari ini? Saat kita mempelajari penggunaan variabel tadi, tidakkah kamu menyadari suatu hal yang luar biasa? Tadi kita melakukan penjumlahan apel dengan jeruk yang jelas tidak mungkin dilakukan di dunia nyata! Keren, bukan?
+
+# Hal tadi mungkin dilakukan menggunakan R karena kebetulan variabel apel dan jeruk sama-sama berisi data angka.
+
+# Apa yang akan terjadi ya kira-kira jika isi dari salah satu variabel apel atau jeruk itu bukan berisi angka? Kamu pasti penasaran kan? Nih aku berikan step by step instruksi yang bisa kamu lakukan ya.
+
+# Kamu masih ingat kan kalau kita memiliki 5 apel dan 7 jeruk? Sekarang coba kamu ganti isi dari variabel jeruk dari 7 menjadi “tujuh”
+# Klik button “Run” dan lihat apa yang terjadi.
+# Pasti muncul error, bukan? Ingat baik-baik error yang muncul ya karena nanti akan aku tanyakan!
+# Sekarang coba kembalikan isi dari variabel jeruk menjadi 7
+# Klik button “Run” lagi dan seharusnya error yang tadi kamu temukan sudah tidak muncul lagi
+# "Submit" jawabanmu
+
+
+#Kamu memiliki 5 apel dan 7 jeruk
+apel <- 5
+jeruk <- 7
+
+#Jumlahkan buah-buahan yang kamu miliki
+apel + jeruk
+
+#Simpan apel + jeruk ke dalam variable 'buah'
+buah <- apel + jeruk
