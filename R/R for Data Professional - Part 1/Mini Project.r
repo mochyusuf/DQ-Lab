7e3bddf6fbee2f30fb7d1e3f844b6551cf484ce7
Mochamad Yusuf <mochyusuf120@gmail.com>
2025-05-29 06:17:49 +0700

    Mini Project

---
+++ R/R for Data Professional - Part 1/Mini Project.r
@@ -0,0 +1,53 @@
+# Sekitar pukul setengah enam sore, aku baru selesai menuntaskan soal-soal dan pembelajaran hari ini. Aku pun mulai merapikan meja ketika Andra muncul dari belakangku dengan buku catatannya. 
+
+ 
+
+# Ketika aku membuka dan membacanya, ternyata sudah ada soal berdasarkan materi yang sudah aku pelajari seharian ini, dengan notes dari Andra untuk menguji pemahamanku.
+
+ 
+
+# “Terima kasih untuk hari ini. Sesuai janjiku, aku bakal kasih kamu rangkuman belajar, hehehe. Tapi tentunya ada PR buat dikerjain,” katanya lagi. 
+
+# “Siap.”
+
+ 
+
+# “Tenang aja, masih bisa kamu kerjakan besok kok. Hati-hati pulangnya! Satu lagi, aku senang kamu tipe pembelajar yang cepat dan gesit.”
+
+ 
+
+# Andra pun berlalu pergi. Karena sudah banyak hal yang kupelajari hari ini, aku yakin bisa mengerjakannya dengan baik untuk tugas-tugas barunya. Karena aku sudah belajar dengan sungguh-sungguh aku yakin bisa mengerjakannya dengan baik.
+
+ 
+
+# Tugas dari Andra:
+
+# Buatlah sebuah variabel bernama hari_pahlawan dan isi dengan "Nov 10, 2021" menggunakan format tanggal yang sudah dipelajari sebelumnya dan tampilkan hasilnya
+# Buatlah sebuah variabel bernama radius dan isi dengan angka 15
+# Dengan menggunakan variabel radius tersebut, hitung luas sebuah lingkaran (pi = 3.14) dan simpan hasilnya ke dalam variabel bernama luas_lingkaran. Notes: Rumus Luas Lingkaran = pi * r ^ 2
+# Tampilkan nilai dari luas_lingkaran
+# Tampilkan pembulatan ke atas dari luas_lingkaran
+# Tampilkan pembulatan ke bawah dari luas_lingkaran
+
+
+
+#variable hari_pahlawan
+hari_pahlawan <- as.Date("Nov 10, 2021", "%b %d, %Y")
+ 
+#menampilkan variable hari_pahlawan
+hari_pahlawan
+ 
+#variable radius
+radius <- 15
+ 
+#variable luas_lingkaran
+luas_lingkaran <- 3.14 * radius ^ 2
+ 
+#menampilkan variable luas_lingkaran
+luas_lingkaran
+ 
+#pembulatan ke atas luas_lingkaran
+ceiling(luas_lingkaran)
+ 
+#pembulatan ke bawah luas_lingkaran
+floor(luas_lingkaran)
